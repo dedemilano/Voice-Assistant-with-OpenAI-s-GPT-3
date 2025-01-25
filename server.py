68ed63c3730cde5e1ba3db888ad29edeac6aa0bf
Deoth <gueideoth@gmail.com>
2025-01-25 05:42:38 -0500

    Implement speech-to-text and message processing routes with JSON responses

--- server.py
+++ server.py
@@ -16,18 +16,44 @@ def index():
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    audio_binary = request.data 
+    text = speech_to_text(audio_binary)
 
+    response = app.response_class(
+        response = json.dumps({"text": text}),
+        status = 200,
+        mimetype= 'application/json'
+    )
+
+    print(response)
+    print(response.data)
+
+    return response
 
 @app.route('/process-message', methods=['POST'])
 def process_prompt_route():
-    response = app.response_class(
-        response=json.dumps({"openaiResponseText": None, "openaiResponseSpeech": None}),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    
+    user_message = request.json['userMessage']
+    print('user_message' , user_message)
 
+    voice = request.json['voice']
+    print('voice', voice)
+
+    openai_response_text = openai_process_message(user_message)
+    openai_response_text = os.linesep.join([s for s in openai_response_text.splitlines() if s])
+
+    openai_response_speech = text_to_speech(openai_response_text, voice)
+
+    openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
+    response = app.response_class(
+        response = json.dumps({"openaiResponseText": openai_response_text, "openaiResponseSpeech": openai_response_speech}),
+        status = 200,
+        mimetype= 'application/json'
+    )
+
+    print(response)
+    return response
 
 if __name__ == "__main__":
     app.run(port=8000, host='0.0.0.0')
